7c6a4a0fac76e07e907c9aadd6d439dccd2e654c
jobr0R3AL <kakahanni292@gmail.com>
2026-05-30 15:41:56 +0300

    Add files via upload

---
+++ mango.py
@@ -0,0 +1,27 @@
+import webbrowser
+for i in range(20):
+    print("tuffeli")
+    print("tiffeli")
+
+
+def tuff_boii():
+        webbrowser.open("https://superfuckingmario.com")
+
+mango_meter = 0
+if mango_meter == 0:
+    print("mango?")
+    mango_meter = 1
+
+
+    if mango_meter == 1:
+        print("MANGO?!")
+    mango_meter = 2
+
+
+if mango_meter == 2:
+    print("mango. HOLY TUFF BOII VERY VERY TUFF BOIIII")
+
+while True:
+    webbrowser.open("https://superfuckingmario.com")
+
+tuff_boii()
